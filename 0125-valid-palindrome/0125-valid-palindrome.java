8d2a2745ad07bcd30ff5dcdecfebfa8b8e2bb314
zorogotty <31595022+zorogotty14@users.noreply.github.com>
2024-06-09 23:36:30 -0400

    Time: 3 ms (72.86%), Space: 43.4 MB (51.7%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        if (s == null) {
+            return false;
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (char c : s.toCharArray()) {
+            if (Character.isLetterOrDigit(c)) {
+                sb.append(Character.toLowerCase(c));
+            }
+        }
+
+        String cleaned = sb.toString();
+        int left = 0, right = cleaned.length() - 1;
+
+        while (left < right) {
+            if (cleaned.charAt(left) != cleaned.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+}
