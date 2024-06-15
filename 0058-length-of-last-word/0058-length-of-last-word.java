6a79e548e3fec218ef2dca682eb8742495c3724d
zorogotty <31595022+zorogotty14@users.noreply.github.com>
2024-06-15 16:33:55 -0400

    Time: 0 ms (100%), Space: 41.7 MB (36.62%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.java
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -1,8 +1,8 @@
 class Solution {
     public int lengthOfLastWord(String s) {
         s = s.trim();
-        String[] words = s.split("\\s+");
-        String lastWord = words[words.length - 1];
-        return lastWord.length();
+        
+        int lastSpaceIndex = s.lastIndexOf(' ');
+        return s.length() - lastSpaceIndex - 1;
     }
 }
