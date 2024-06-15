f7412592c7a8ca921534ddd90f6631b80b283f55
zorogotty <31595022+zorogotty14@users.noreply.github.com>
2024-06-15 16:40:50 -0400

    Time: 5 ms (70.56%), Space: 45.3 MB (42.14%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.java
+++ 0006-zigzag-conversion/0006-zigzag-conversion.java
@@ -1,17 +1,16 @@
 class Solution {
     public String convert(String s, int numRows) {
-        if (numRows == 1 || s.length() <= numRows) {
+        if (numRows == 1) {
             return s;
         }
-        
         StringBuilder[] rows = new StringBuilder[numRows];
         for (int i = 0; i < numRows; i++) {
             rows[i] = new StringBuilder();
         }
-        
+
         int currentRow = 0;
         boolean goingDown = false;
-        
+
         for (char c : s.toCharArray()) {
             rows[currentRow].append(c);
             if (currentRow == 0 || currentRow == numRows - 1) {
@@ -19,12 +18,12 @@ class Solution {
             }
             currentRow += goingDown ? 1 : -1;
         }
-        
+
         StringBuilder result = new StringBuilder();
         for (StringBuilder row : rows) {
             result.append(row);
         }
-        
+
         return result.toString();
     }
 }
