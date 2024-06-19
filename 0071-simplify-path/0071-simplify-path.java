a94de5b5695c8a8a448d4e3f274f1ff630019628
zorogotty <31595022+zorogotty14@users.noreply.github.com>
2024-06-19 18:09:40 -0400

    Time: 4 ms (84.74%), Space: 43.2 MB (66.86%) - LeetHub

---
+++ 0071-simplify-path/0071-simplify-path.java
@@ -0,0 +1,23 @@
+class Solution {
+    public String simplifyPath(String path) {
+        Stack<String> stack = new Stack<>();
+        String[] components = path.split("/");
+
+        for (String component : components) {
+            if (component.equals("..")) {
+                if (!stack.isEmpty()) {
+                    stack.pop();
+                }
+            } else if (!component.isEmpty() && !component.equals(".")) {
+                stack.push(component);
+            }
+        }
+
+        StringBuilder simplifiedPath = new StringBuilder();
+        for (String dir : stack) {
+            simplifiedPath.append("/").append(dir);
+        }
+
+        return simplifiedPath.length() > 0 ? simplifiedPath.toString() : "/";
+    }
+}
