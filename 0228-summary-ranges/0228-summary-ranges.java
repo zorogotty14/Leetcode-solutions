e80a27ec638d8b222b8586eabaef5f016487080b
zorogotty <31595022+zorogotty14@users.noreply.github.com>
2024-06-18 21:23:35 -0400

    Time: 7 ms (7.69%), Space: 41.8 MB (14.3%) - LeetHub

--- 0228-summary-ranges/0228-summary-ranges.java
+++ 0228-summary-ranges/0228-summary-ranges.java
@@ -1,30 +1,28 @@
 class Solution {
     public List<String> summaryRanges(int[] nums) {
         List<String> result = new ArrayList<>();
-        if (nums.length == 0) {
+        int n = nums.length;
+        if (n == 0) {
             return result;
         }
 
         int start = nums[0];
-        int end = nums[0];
-
-        for (int i = 1; i < nums.length; i++) {
-            if (nums[i] != end + 1) {
-                if (start == end) {
+        
+        for (int i = 1; i < n; i++) {
+            if (nums[i] != nums[i - 1] + 1) {
+                if (start == nums[i - 1]) {
                     result.add(String.valueOf(start));
                 } else {
-                    result.add(start + "->" + end);
+                    result.add(start + "->" + nums[i - 1]);
                 }
                 start = nums[i];
             }
-            end = nums[i];
         }
 
-        // Add the final range
-        if (start == end) {
+        if (start == nums[n - 1]) {
             result.add(String.valueOf(start));
         } else {
-            result.add(start + "->" + end);
+            result.add(start + "->" + nums[n - 1]);
         }
 
         return result;
