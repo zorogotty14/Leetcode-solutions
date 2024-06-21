b6846f5eaedbb65d11f21f1190825e43a73f7194
zorogotty <31595022+zorogotty14@users.noreply.github.com>
2024-06-20 23:45:47 -0400

    Time: 8 ms (75.1%), Space: 43.5 MB (67.77%) - LeetHub

---
+++ 0224-basic-calculator/0224-basic-calculator.java
@@ -0,0 +1,40 @@
+class Solution {
+    public int calculate(String s) {
+        Stack<Integer> stack = new Stack<>();
+        int result = 0;
+        int number = 0;
+        int sign = 1; // 1 means positive, -1 means negative
+        
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            
+            if (Character.isDigit(c)) {
+                number = number * 10 + (c - '0');
+            } else if (c == '+') {
+                result += sign * number;
+                number = 0;
+                sign = 1;
+            } else if (c == '-') {
+                result += sign * number;
+                number = 0;
+                sign = -1;
+            } else if (c == '(') {
+                stack.push(result);
+                stack.push(sign);
+                result = 0;
+                sign = 1;
+            } else if (c == ')') {
+                result += sign * number;
+                number = 0;
+                result *= stack.pop(); // pop the sign
+                result += stack.pop(); // pop the result before the parenthesis
+            }
+        }
+        
+        if (number != 0) {
+            result += sign * number;
+        }
+        
+        return result;
+    }
+}
