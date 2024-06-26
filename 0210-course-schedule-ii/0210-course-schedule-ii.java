e6a4d459c7ecb07a9063c7afa5d086e386072179
zorogotty <31595022+zorogotty14@users.noreply.github.com>
2024-06-26 01:15:11 -0400

    Time: 5 ms (76.32%), Space: 45.8 MB (40.37%) - LeetHub

---
+++ 0210-course-schedule-ii/0210-course-schedule-ii.java
@@ -0,0 +1,46 @@
+class Solution {
+    public int[] findOrder(int numCourses, int[][] prerequisites) {
+        // Create an adjacency list for the graph
+        List<List<Integer>> graph = new ArrayList<>();
+        int[] inDegree = new int[numCourses];
+        
+        for (int i = 0; i < numCourses; i++) {
+            graph.add(new ArrayList<>());
+        }
+        
+        for (int[] prereq : prerequisites) {
+            graph.get(prereq[1]).add(prereq[0]);
+            inDegree[prereq[0]]++;
+        }
+        
+        // Queue for nodes with no incoming edges
+        Queue<Integer> queue = new LinkedList<>();
+        for (int i = 0; i < numCourses; i++) {
+            if (inDegree[i] == 0) {
+                queue.offer(i);
+            }
+        }
+        
+        List<Integer> order = new ArrayList<>();
+        while (!queue.isEmpty()) {
+            int course = queue.poll();
+            order.add(course);
+            for (int neighbor : graph.get(course)) {
+                inDegree[neighbor]--;
+                if (inDegree[neighbor] == 0) {
+                    queue.offer(neighbor);
+                }
+            }
+        }
+        
+        if (order.size() == numCourses) {
+            int[] result = new int[numCourses];
+            for (int i = 0; i < numCourses; i++) {
+                result[i] = order.get(i);
+            }
+            return result;
+        } else {
+            return new int[0]; // Cycle detected, cannot finish all courses
+        }
+    }
+}
