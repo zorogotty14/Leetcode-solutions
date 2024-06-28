279a88d586750f5047b80f439b55a023cd0fa047
zorogotty <31595022+zorogotty14@users.noreply.github.com>
2024-06-27 20:45:33 -0400

    Time: 14 ms (94.26%), Space: 94.4 MB (63.64%) - LeetHub

--- 0077-combinations/0077-combinations.java
+++ 0077-combinations/0077-combinations.java
@@ -4,15 +4,16 @@ class Solution {
         backtrack(result, new ArrayList<>(), 1, n, k);
         return result;
     }
+
     private void backtrack(List<List<Integer>> result, List<Integer> tempList, int start, int n, int k) {
         if (tempList.size() == k) {
             result.add(new ArrayList<>(tempList));
             return;
         }
-        for (int i = start; i <= n; i++) {
+        for (int i = start; i <= n - (k - tempList.size()) + 1; i++) {  // Early termination
             tempList.add(i);
             backtrack(result, tempList, i + 1, n, k);
-            tempList.remove(tempList.size() - 1);
+            tempList.remove(tempList.size() - 1);  // Backtrack
         }
     }
 }
