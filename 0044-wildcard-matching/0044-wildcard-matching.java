e80a5cc8f3c5aa4581b34740114da491e5dd9218
zorogotty <31595022+zorogotty14@users.noreply.github.com>
2024-07-28 20:09:01 +0530

    Time: 23 ms (52.02%), Space: 44.4 MB (81.20%) - LeetHub

---
+++ 0044-wildcard-matching/0044-wildcard-matching.java
@@ -0,0 +1,32 @@
+class Solution {
+    public boolean isMatch(String s, String p) {
+        int m = s.length();
+        int n = p.length();
+        boolean[][] dp = new boolean[m + 1][n + 1];
+
+        // Initialization
+        dp[0][0] = true;
+
+        // Fill the first row (when the string is empty)
+        for (int j = 1; j <= n; j++) {
+            if (p.charAt(j - 1) == '*') {
+                dp[0][j] = dp[0][j - 1];
+            }
+        }
+
+        // Fill the rest of the dp table
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (p.charAt(j - 1) == '*') {
+                    // '*' can match any sequence: empty sequence (dp[i][j-1]) or one more character (dp[i-1][j])
+                    dp[i][j] = dp[i][j - 1] || dp[i - 1][j];
+                } else if (p.charAt(j - 1) == '?' || p.charAt(j - 1) == s.charAt(i - 1)) {
+                    // '?' matches any single character or exact match
+                    dp[i][j] = dp[i - 1][j - 1];
+                }
+            }
+        }
+
+        return dp[m][n];
+    }
+}
