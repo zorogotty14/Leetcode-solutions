08d0756f1ca8b94cdc68f4010709cb608ab10409
zorogotty <31595022+zorogotty14@users.noreply.github.com>
2024-07-29 11:03:09 +0530

    Time: 15 ms (88.75%), Space: 41.7 MB (99.30%) - LeetHub

--- 1395-count-number-of-teams/1395-count-number-of-teams.java
+++ 1395-count-number-of-teams/1395-count-number-of-teams.java
@@ -1,17 +1,36 @@
 class Solution {
     public int numTeams(int[] rating) {
         int n = rating.length;
+        if (n < 3) {
+            return 0;
+        }
+
         int count = 0;
 
-        for (int i = 0; i < n - 2; i++) {
-            for (int j = i + 1; j < n - 1; j++) {
-                for (int k = j + 1; k < n; k++) {
-                    if ((rating[i] < rating[j] && rating[j] < rating[k]) ||
-                        (rating[i] > rating[j] && rating[j] > rating[k])) {
-                        count++;
-                    }
+        for (int j = 1; j < n - 1; j++) {
+            int leftLess = 0, leftGreater = 0;
+            int rightLess = 0, rightGreater = 0;
+
+            // Count soldiers to the left of j
+            for (int i = 0; i < j; i++) {
+                if (rating[i] < rating[j]) {
+                    leftLess++;
+                } else if (rating[i] > rating[j]) {
+                    leftGreater++;
                 }
             }
+
+            // Count soldiers to the right of j
+            for (int k = j + 1; k < n; k++) {
+                if (rating[k] < rating[j]) {
+                    rightLess++;
+                } else if (rating[k] > rating[j]) {
+                    rightGreater++;
+                }
+            }
+
+            // Calculate the number of valid teams with soldier j in the middle
+            count += leftLess * rightGreater + leftGreater * rightLess;
         }
 
         return count;
