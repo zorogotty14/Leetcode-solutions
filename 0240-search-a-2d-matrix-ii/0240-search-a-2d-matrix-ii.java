fa56bc46d6a014da5df5aac45c7da82488f113e3
zorogotty <31595022+zorogotty14@users.noreply.github.com>
2024-08-23 10:05:32 -0400

    Time: 6 ms (58.04%), Space: 46.3 MB (6.89%) - LeetHub

---
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.java
@@ -0,0 +1,29 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
+            return false;
+        }
+
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+
+        // Start from the top-right corner
+        int row = 0;
+        int col = cols - 1;
+
+        while (row < rows && col >= 0) {
+            if (matrix[row][col] == target) {
+                return true;
+            } else if (matrix[row][col] > target) {
+                // Move left
+                col--;
+            } else {
+                // Move down
+                row++;
+            }
+        }
+
+        // Target not found
+        return false;
+    }
+}
