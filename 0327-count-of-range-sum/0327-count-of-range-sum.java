fc2c379e16a2fd26f2ad95715abb2ca817c0f7a3
zorogotty <31595022+zorogotty14@users.noreply.github.com>
2024-09-01 02:44:32 -0400

    Time: 52 ms (72.15%), Space: 59.6 MB (74.8%) - LeetHub

---
+++ 0327-count-of-range-sum/0327-count-of-range-sum.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int countRangeSum(int[] nums, int lower, int upper) {
+        long[] prefixSums = new long[nums.length + 1];
+        for (int i = 0; i < nums.length; i++) {
+            prefixSums[i + 1] = prefixSums[i] + nums[i];
+        }
+        return countWhileMergeSort(prefixSums, 0, prefixSums.length, lower, upper);
+    }
+
+    private int countWhileMergeSort(long[] sums, int start, int end, int lower, int upper) {
+        if (end - start <= 1) return 0;
+        int mid = (start + end) / 2;
+        int count = countWhileMergeSort(sums, start, mid, lower, upper) 
+                  + countWhileMergeSort(sums, mid, end, lower, upper);
+        int j = mid, k = mid, t = mid;
+        long[] cache = new long[end - start];
+        int r = 0;
+
+        for (int i = start; i < mid; i++) {
+            while (k < end && sums[k] - sums[i] < lower) k++;
+            while (j < end && sums[j] - sums[i] <= upper) j++;
+            while (t < end && sums[t] < sums[i]) cache[r++] = sums[t++];
+            cache[r++] = sums[i];
+            count += j - k;
+        }
+        System.arraycopy(cache, 0, sums, start, t - start);
+        return count;
+    }
+}
