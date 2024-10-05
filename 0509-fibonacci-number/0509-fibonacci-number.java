76e42d53a9c634f3e867e7a672a2f105acb65e00
zorogotty <31595022+zorogotty14@users.noreply.github.com>
2024-10-05 08:06:54 -0400

    Time: 0 ms (100%), Space: 40.1 MB (66.35%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int fib(int n) {
+        if (n == 0) return 0;
+        if (n == 1) return 1;
+        
+        int prev1 = 0; // F(0)
+        int prev2 = 1; // F(1)
+        
+        for (int i = 2; i <= n; i++) {
+            int current = prev1 + prev2; // F(i) = F(i-1) + F(i-2)
+            prev1 = prev2;
+            prev2 = current;
+        }
+        
+        return prev2; // The nth Fibonacci number
+    }
+}
