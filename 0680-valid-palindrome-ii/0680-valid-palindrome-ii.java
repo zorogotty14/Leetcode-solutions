ce6333087127d508684bc1179e576edce90cce84
zorogotty <31595022+zorogotty14@users.noreply.github.com>
2024-11-04 17:08:13 -0500

    Time: 4 ms (99.62%), Space: 45.3 MB (45.31%) - LeetHub

---
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.java
@@ -0,0 +1,29 @@
+class Solution {
+    public boolean validPalindrome(String s) {
+        int left = 0;
+        int right = s.length() - 1;
+        
+        while (left < right) {
+            if (s.charAt(left) != s.charAt(right)) {
+                // Check if skipping either the left or right character makes a palindrome
+                return isPalindrome(s, left + 1, right) || isPalindrome(s, left, right - 1);
+            }
+            left++;
+            right--;
+        }
+        
+        return true; // It's already a palindrome if we reach here
+    }
+    
+    // Helper function to check if a substring is a palindrome
+    private boolean isPalindrome(String s, int left, int right) {
+        while (left < right) {
+            if (s.charAt(left) != s.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+}
