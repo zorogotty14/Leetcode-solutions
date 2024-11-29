563a98d16a5cd8fd2d14205f7e0bda9de9432985
zorogotty <31595022+zorogotty14@users.noreply.github.com>
2024-11-29 01:44:49 -0500

    Time: 0 ms (100%), Space: 41.1 MB (24.86%) - LeetHub

---
+++ 0836-rectangle-overlap/0836-rectangle-overlap.java
@@ -0,0 +1,9 @@
+class Solution {
+    public boolean isRectangleOverlap(int[] rec1, int[] rec2) {
+        // Check if one rectangle is to the left, right, above, or below the other
+        return !(rec1[2] <= rec2[0] || // rec1 is to the left of rec2
+                 rec1[0] >= rec2[2] || // rec1 is to the right of rec2
+                 rec1[3] <= rec2[1] || // rec1 is below rec2
+                 rec1[1] >= rec2[3]);  // rec1 is above rec2
+    }
+}
