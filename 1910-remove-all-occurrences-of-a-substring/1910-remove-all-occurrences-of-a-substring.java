4a4b5bb05aef64b11d8cf757e194f62270c5b0fd
zorogotty <31595022+zorogotty14@users.noreply.github.com>
2025-02-11 00:17:45 -0500

    Time: 1 ms (99.46%), Space: 42 MB (88.37%) - LeetHub

---
+++ 1910-remove-all-occurrences-of-a-substring/1910-remove-all-occurrences-of-a-substring.java
@@ -0,0 +1,12 @@
+class Solution {
+    public String removeOccurrences(String s, String part) {
+     StringBuilder sb = new StringBuilder(s);
+        
+        while (sb.indexOf(part) != -1) {
+            int index = sb.indexOf(part);
+            sb.delete(index, index + part.length());
+        }
+        
+        return sb.toString();
+    }
+}
