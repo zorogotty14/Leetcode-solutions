a5b77f46ba6fa3db8b575ff01ed128a1c52a3ec4
zorogotty <31595022+zorogotty14@users.noreply.github.com>
2025-07-31 22:46:15 -0700

    Time: 1 ms (82.66%), Space: 42.2 MB (24.33%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.java
+++ 0118-pascals-triangle/0118-pascals-triangle.java
@@ -1,32 +1,30 @@
 class Solution {
     public List<List<Integer>> generate(int numRows) {
-        List<List<Integer>> triangle = new ArrayList<>();
-        
-        if (numRows == 0) {
-            return triangle;
-        }
-        
-        // Initialize the first row
-        List<Integer> firstRow = new ArrayList<>();
-        firstRow.add(1);
-        triangle.add(firstRow);
-        
-        // Generate each row
-        for (int i = 1; i < numRows; i++) {
-            List<Integer> prevRow = triangle.get(i - 1);
-            List<Integer> newRow = new ArrayList<>();
-            
-            newRow.add(1); // The first element is always 1
-            
-            // Compute the middle elements
-            for (int j = 1; j < i; j++) {
-                newRow.add(prevRow.get(j - 1) + prevRow.get(j));
+        List<List<Integer>> triangle = new ArrayList<List<Integer>>();
+
+        triangle.add(new ArrayList<>());
+        triangle.get(0).add(1);
+
+        for (int rowNum = 1; rowNum < numRows; rowNum++) {
+            List<Integer> row = new ArrayList<>();
+            List<Integer> prevRow = triangle.get(rowNum - 1);
+
+            // The first row element is always 1.
+            row.add(1);
+
+            // Each triangle element (other than the first and last of each row)
+            // is equal to the sum of the elements above-and-to-the-left and
+            // above-and-to-the-right.
+            for (int j = 1; j < rowNum; j++) {
+                row.add(prevRow.get(j - 1) + prevRow.get(j));
             }
-            
-            newRow.add(1); // The last element is always 1
-            triangle.add(newRow);
+
+            // The last row element is always 1.
+            row.add(1);
+
+            triangle.add(row);
         }
-        
+
         return triangle;
     }
 }
