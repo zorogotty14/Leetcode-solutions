086f80f6cc5da4f50bb00ab2fbfddaf1d718923e
zorogotty <31595022+zorogotty14@users.noreply.github.com>
2025-09-22 22:06:06 -0700

    Time: 0 ms (100%), Space: 41.4 MB (93.05%) - LeetHub

--- 0165-compare-version-numbers/0165-compare-version-numbers.java
+++ 0165-compare-version-numbers/0165-compare-version-numbers.java
@@ -1,24 +1,28 @@
-public class Solution {
+class Solution {
     public int compareVersion(String version1, String version2) {
-        // Split the version strings by '.'
-        String[] v1 = version1.split("\\.");
-        String[] v2 = version2.split("\\.");
-        
-        int len = Math.max(v1.length, v2.length);
-        
-        // Compare each revision one by one
-        for (int i = 0; i < len; i++) {
-            int rev1 = i < v1.length ? Integer.parseInt(v1[i]) : 0;
-            int rev2 = i < v2.length ? Integer.parseInt(v2[i]) : 0;
-            
-            if (rev1 < rev2) {
-                return -1;
-            } else if (rev1 > rev2) {
-                return 1;
+        int i1 = 0, i2 = 0;
+        int n1 = version1.length(), n2 = version2.length();
+
+        while (i1 < n1 || i2 < n2) {
+            int v1 = 0, v2 = 0;
+
+            while (i1 < n1 && version1.charAt(i1) != '.') {
+                v1 = v1 * 10 + (version1.charAt(i1) - '0');
+                i1++;
             }
+
+            while (i2 < n2 && version2.charAt(i2) != '.') {
+                v2 = v2 * 10 + (version2.charAt(i2) - '0');
+                i2++;
+            }
+
+            if (v1 < v2) return -1;
+            if (v1 > v2) return 1;
+
+            i1++; // skip '.'
+            i2++;
         }
-        
-        // If all revisions are the same, return 0
+
         return 0;
     }
 }
