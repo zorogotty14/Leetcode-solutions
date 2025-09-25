663140324f29039e6f4a12e203f734d2e81fc44f
zorogotty <31595022+zorogotty14@users.noreply.github.com>
2025-09-25 10:46:14 -0700

    Time: 10 ms (6%), Space: 45.7 MB (20.7%) - LeetHub

--- 0120-triangle/0120-triangle.java
+++ 0120-triangle/0120-triangle.java
@@ -1,21 +1,11 @@
 class Solution {
-    public int minimumTotal(List<List<Integer>> triangle) {
-        int n = triangle.size();
-        int[] dp = new int[n];
-        
-        // Initialize dp with the last row of the triangle
-        for (int i = 0; i < n; i++) {
-            dp[i] = triangle.get(n - 1).get(i);
-        }
-        
-        // Iterate from the second last row to the top row
-        for (int row = n - 2; row >= 0; row--) {
-            for (int col = 0; col <= row; col++) {
-                dp[col] = triangle.get(row).get(col) + Math.min(dp[col], dp[col + 1]);
-            }
-        }
-        
-        // The first element will have the minimum path sum
-        return dp[0];
+    public int minimumTotal(List<List<Integer>> tri) {
+        for (int i = tri.size() - 2; i >= 0; i--)
+            for (int j = 0; j < tri.get(i).size(); j++)
+                tri.get(i).set(j, tri.get(i).get(j) + Math.min(
+                    tri.get(i + 1).get(j),
+                    tri.get(i + 1).get(j + 1)
+                ));
+        return tri.get(0).get(0);
     }
 }
