e0351a996603dad9054121f1b016c50503de2d33
zorogotty <31595022+zorogotty14@users.noreply.github.com>
2025-11-10 22:28:24 -0700

    Time: 19 ms (63.87%), Space: 43.4 MB (52.31%) - LeetHub

--- 0474-ones-and-zeroes/0474-ones-and-zeroes.java
+++ 0474-ones-and-zeroes/0474-ones-and-zeroes.java
@@ -1,26 +1,15 @@
 class Solution {
-    public int findMaxForm(String[] strs, int m, int n) {
-        // DP array to store the maximum number of strings that can be formed with i 0's and j 1's
-        int[][] dp = new int[m + 1][n + 1];
-        
-        // Iterate over each string in strs
-        for (String str : strs) {
-            // Count number of 0's and 1's in the current string
+    public int findMaxForm(String[] S, int M, int N) {
+        int[][] dp = new int[M+1][N+1];
+        for (String str : S) {
             int zeros = 0, ones = 0;
-            for (char c : str.toCharArray()) {
+            for (char c : str.toCharArray())
                 if (c == '0') zeros++;
                 else ones++;
-            }
-            
-            // Update the dp table from bottom-right to top-left
-            for (int i = m; i >= zeros; i--) {
-                for (int j = n; j >= ones; j--) {
-                    dp[i][j] = Math.max(dp[i][j], dp[i - zeros][j - ones] + 1);
-                }
-            }
+            for (int i = M; i >= zeros; i--)
+                for (int j = N; j >= ones; j--)
+                    dp[i][j] = Math.max(dp[i][j], dp[i-zeros][j-ones] + 1);
         }
-        
-        // The answer is in dp[m][n]
-        return dp[m][n];
+        return dp[M][N];
     }
 }
