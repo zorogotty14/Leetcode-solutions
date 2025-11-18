6fc15d17294787a6572a89b706cac9e5fab87b25
zorogotty <31595022+zorogotty14@users.noreply.github.com>
2025-11-17 19:17:29 -0700

    Time: 0 ms (100%), Space: 43.8 MB (24.63%) - LeetHub

--- 0717-1-bit-and-2-bit-characters/0717-1-bit-and-2-bit-characters.java
+++ 0717-1-bit-and-2-bit-characters/0717-1-bit-and-2-bit-characters.java
@@ -2,19 +2,8 @@ class Solution {
     public boolean isOneBitCharacter(int[] bits) {
         int n = bits.length;
         int i = 0;
-
-        // Traverse the array until the second-to-last bit
-        while (i < n - 1) {
-            if (bits[i] == 1) {
-                // If it's a '1', move by two steps (two-bit character)
-                i += 2;
-            } else {
-                // If it's a '0', move by one step (one-bit character)
-                i++;
-            }
-        }
-
-        // If we end at the last bit (index n-1), it's a one-bit character
+        while(i < n - 1)
+            i += bits[i] + 1;
         return i == n - 1;
     }
 }
